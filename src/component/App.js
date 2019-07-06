8eb77c19bc5ea76628400d29464bf071d28a3945
ueno <ueno@ue-2.local>
2019-07-06 20:28:01 +0900

    bootstrap and event form

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <>
-      template
-    </>
+    <div className="container-fluid">
+      <h4>イベント作成</h4>
+      <form>
+        <div className='form-group'>
+          <label htmlFor='formEventTitle'>タイトル</label>
+          <input className='form-control' id='formEventTitle' />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='formEventBody'>ボディー</label>
+          <input className='form-control' id='formEventBody' />
+        </div>
+
+        <button className='btn btn-primary'>イベント作成</button>
+        <button className='btn btn-danger'>すべてのイベント削除</button>
+      </form>
+
+      <h4>イベント一覧</h4>
+      <table className='table table-hover'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
