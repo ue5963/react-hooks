1edae031a9238bdbc8ba4f64ea17c7283585f467
ueno <ueno@ue-2.local>
2019-07-07 08:45:36 +0900

    refactor component

--- src/component/App.js
+++ src/component/App.js
@@ -1,66 +1,17 @@
-import React, { useState, useReducer } from 'react'
+import React, { useReducer } from 'react'
 import reducer from '../reducers'
+
+import EventForm from './EventForm'
 import Event from './Event'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App(props) {
+function App() {
   const [state, dispatch] = useReducer(reducer, [])
-
-  const [_state, setState] = useState(props)
-  const {title, body} = _state
-
-   const addEvent = e => {
-     e.preventDefault()
-
-      dispatch({
-        type: 'CREATE_EVENT',
-        title,
-        body,
-      })
-
-     // Clear input title, body
-     setState(props)
-   }
-
-  const removeAllEvent = e => {
-    e.preventDefault()
-
-    const result = window.confirm('Do you remove all event')
-    if (!result) {
-      return
-    }
-
-    dispatch({
-      type: 'DELETE_ALL_EVENT',
-    })
-  }
-
-  const changeTitle = title => {
-    setState({ ..._state, title })
-  }
-
-  const changeBody = (body) => {
-    setState({ ..._state, body })
-  }
 
   return (
     <div className="container-fluid">
-      <h4>イベント作成</h4>
-      <form>
-        <div className='form-group'>
-          <label htmlFor='formEventTitle'>Title</label>
-          <input className='form-control' id='formEventTitle' value={title} onChange={e => changeTitle(e.target.value)} />
-        </div>
-
-        <div className='form-group'>
-          <label htmlFor='formEventBody'>Body</label>
-          <input className='form-control' id='formEventBody' value={body} onChange={e => changeBody(e.target.value)} />
-        </div>
-
-        <button className='btn btn-primary' onClick={addEvent} disabled={!title || !body}>イベント作成</button>
-        <button className='btn btn-danger' onClick={removeAllEvent} disabled={state.length === 0}>すべてのイベント削除</button>
-      </form>
+      <EventForm state={state} dispatch={dispatch} />
 
       <h4>イベント一覧</h4>
       <table className='table table-hover'>
@@ -78,11 +29,6 @@ function App(props) {
       </table>
     </div>
   )
-}
-
-App.defaultProps = {
-  title: '',
-  body: '',
 }
 
 export default App
---
+++ src/component/EventForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+
+const EventForm = props => {
+
+  const { state, dispatch } = props
+  const [_state, setState] = useState(props)
+
+  const { title, body } = _state
+
+  const addEvent = e => {
+    e.preventDefault()
+
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body,
+    })
+
+    // Clear input title, body
+    setState({ title: '', body: '' })
+  }
+
+  const removeAllEvent = e => {
+    e.preventDefault()
+
+    const result = window.confirm('Do you remove all event')
+    if (!result) {
+      return
+    }
+
+    dispatch({
+      type: 'DELETE_ALL_EVENT',
+    })
+  }
+
+  const changeTitle = title => {
+    setState({ ..._state, title })
+  }
+
+  const changeBody = (body) => {
+    setState({ ..._state, body })
+  }
+
+
+
+  return (
+    <>
+      <h4>イベント作成</h4>
+      <form>
+        <div className='form-group'>
+          <label htmlFor='formEventTitle'>Title</label>
+          <input className='form-control' id='formEventTitle' value={title} onChange={e => changeTitle(e.target.value)} />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='formEventBody'>Body</label>
+          <input className='form-control' id='formEventBody' value={body} onChange={e => changeBody(e.target.value)} />
+        </div>
+
+        <button className='btn btn-primary' onClick={addEvent} disabled={!title || !body}>イベント作成</button>
+        <button className='btn btn-danger' onClick={removeAllEvent} disabled={state.length === 0}>すべてのイベント削除</button>
+      </form>
+    </>
+  )
+}
+
+EventForm.defaultProps = {
+  title: '',
+  body: '',
+}
+
+export default EventForm
